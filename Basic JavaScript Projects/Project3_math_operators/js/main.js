638d30dc48a7ac7facab989bd0656266e0863b60
rogue1882 <88951456+rogue1882@users.noreply.github.com>
2021-09-16 19:48:46 -0500

    Math operator submission

--- Basic JavaScript Projects/Project3_math_operators/js/main.js
+++ Basic JavaScript Projects/Project3_math_operators/js/main.js
@@ -1,6 +1,40 @@
-function subtraction_function() { //subtraction function code
+function subtract() { //subtraction function code
     var Subtraction = 5-2;
-        document.getElementById("math").innerHTML ="5-2 =" + Subtraction;
-         } 
+    document.getElementById("math").innerHTML ="5-2 =" + Subtraction;
+     }  
+
+function multiplication() { // class multiplication format
+     var simple_math = 6*8;
+     document.getElementById("math").innerHTML = "6 x 8 =" + simple_math;
+    }  
+
+function add() {//Addition function
+    var addition = 100+59; 
+     document.getElementById("math").innerHTML ="100+59 =" + addition; //event
+        }
+
+function division() { //class division format
+    var simple_math = 48/6;
+    document.getElementById("math").innerHTML="48/6 =" + simple_math;
+     } 
+            
+            
+function step_up() {
+    var X=5;  //Increment syntax built as a function
+     X++;
+    document.write(X) ;
+            }
+
+
+function step_down() {
+    var X= 5;  //decrement syntax built as a function
+    X--;
+     document.write(X) ;
+     }  
+     
+function math_sqrt() {
+    var sqrt = Math.sqrt(9); //math object method
+document.write(sqrt);
+}
+   
              
-        
